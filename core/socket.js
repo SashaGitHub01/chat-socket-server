d0b605f4098018eee5b895c2b2656526117c0d07
SashaGitHub01 <sashab4b1n@yandex.ru>
2022-01-20 18:03:48 +0300

    online/offliene behavior

--- core/socket.js
+++ core/socket.js
@@ -1,5 +1,4 @@
 const { Server } = require("socket.io");
-const User = require('../models/User')
 
 
 module.exports = (http) => {
@@ -10,18 +9,57 @@ module.exports = (http) => {
       }
    })
 
+   let onlineUsers = [];
+
+   const addUser = (id, socket) => {
+      if (!onlineUsers.includes(user => user.id === id)) {
+         onlineUsers.push({ id, socket });
+      }
+   }
+
+   const removeUser = (socket) => {
+      const i = onlineUsers.findIndex((user) => user.socket === socket);
+
+      if (i) onlineUsers.splice(i, 1);
+   }
+
+   const getUser = (id) => {
+      return onlineUsers.find(user => user.id === id)
+   }
+
    io.on('connection', (socket) => {
       socket.on('DIALOG:JOIN', (id) => {
          socket.join(id)
-         console.log(socket.rooms)
       })
 
-      socket.on('ONLINE', async (id) => {
+      socket.on('USER:ONLINE', async (id) => {
+         addUser(id, socket.id)
+         io.emit('USER:SET_ONLINE')
+      })
 
+      socket.on('USER:CHECK', async (id, callback) => {
+         const user = getUser(id);
+
+         if (user) {
+            callback(true)
+         } else {
+            callback(false)
+         }
+      })
+
+      socket.on('SEND:MESSAGE', async (creatorId, userId, content) => {
+         const receiver = getUser(userId);
+
+         if (receiver) {
+            io.to(receiver.socket).emit('GET:MESSAGE', { creatorId, userId, content })
+         }
       })
 
       socket.on('disconnect', () => {
          console.log('dis')
+         io.emit('USER:OFFLINE')
+         removeUser(socket.id);
+
       })
    })
 
